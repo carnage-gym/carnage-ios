d46ccfe2515376aad24e4c96e127bc5f2323df03
Diogo <diogo6204@gmail.com>
2024-09-04 21:47:09 +0100

    some progress

--- carnage/ContentView.swift
+++ carnage/ContentView.swift
@@ -9,55 +9,32 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                    .padding(1.0)
-                    
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+        VStack(content: {
+            Text("Carnage")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+            Spacer()
+                        
+            HStack(content: {
+                Text("Test")
+                Button {
+                    print("Pressed!")
+                } label: {
+                    Image(systemName: "gym.bag.fill")
+                        .font(.system(size: 22))
+                        .foregroundStyle(.white)
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                        .background(Color(red: 1, green: 90/255, blue: 90/255))
+                        .clipShape(Capsule())
+                }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                
+                Text("Test")
+            })
+        })
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
