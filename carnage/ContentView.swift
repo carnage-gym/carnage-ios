6e57f6a2d27d337cfd57a3e13bd620afb287ef6e
Diogo <diogo6204@gmail.com>
2024-09-05 10:54:37 +0100

    wrote a bit more of the main page

--- carnage/ContentView.swift
+++ carnage/ContentView.swift
@@ -9,29 +9,36 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    
+    @State var showMenu = false
     var body: some View {
-        VStack(content: {
+        let content = VStack(content: {
             Text("Carnage")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
             Spacer()
-                        
-            HStack(content: {
-                Text("Test")
-                Button {
-                    print("Pressed!")
-                } label: {
-                    Image(systemName: "gym.bag.fill")
-                        .font(.system(size: 22))
-                        .foregroundStyle(.white)
-                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
-                        .background(Color(red: 1, green: 90/255, blue: 90/255))
-                        .clipShape(Capsule())
-                }.padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-                
-                Text("Test")
-            })
+            Divider()
         })
+        
+        let button = Button {
+            showMenu = !showMenu
+        } label: {
+            Image(systemName: "gym.bag.fill")
+                .font(.system(size: 24))
+                .foregroundStyle(.white)
+                .padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
+                .background(showMenu ? Color(red: 1, green: 80/255, blue: 80/255) : Color(red: 1, green: 90/255, blue: 90/255))
+                .clipShape(Circle())
+        }
+        
+        if showMenu {
+            ZStack {
+                Text("\(Image(systemName: "info.circle")) menu options will appear here... ").bold()
+                content.opacity(0.4)
+            }
+        } else {
+            content
+        }
+        button
+        
     }
 }
 
