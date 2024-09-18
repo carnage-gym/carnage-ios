1207101aad58f3dde0c505a33dcff3c909b8cd1d
Diogo <diogo6204@gmail.com>
2024-09-18 14:09:39 +0100

    forgor

--- carnage/views/ProfileView.swift
+++ carnage/views/ProfileView.swift
@@ -6,11 +6,24 @@
 //
 
 import SwiftUI
+import KeychainWrapper
 
 struct ProfileView: View {
+    private let keychain: KeychainWrapper = KeychainWrapper(serviceName: "carnage.carnage")
+    
+    // Wrap this inside an observedobject
+    private var accessToken: String?
+    private var refreshToken: String?
+    
+    private func isSignedIn() -> Bool {
+        guard let token = accessToken else { return false }
+        return !token.isEmpty
+    }
+    
     var body: some View {
         Text("\(Image(systemName: "info.circle")) profile will appear here... ").bold()
     }
+    
 }
 
 #Preview {
